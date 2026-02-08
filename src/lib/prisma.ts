33b3c16cde485acda86328579be386718f351784
よしはしかずき <yoshihashikazuki@yoshihashikazukis-MacBook-Air.local>
2026-02-08 23:15:32 +0900

    fix: Explicitly pass DATABASE_URL to PrismaClient

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,6 +7,11 @@ const globalForPrisma = globalThis as unknown as {
 const prismaClientSingleton = () => {
   return new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   })
 }
 
